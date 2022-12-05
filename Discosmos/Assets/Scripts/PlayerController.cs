77bfd64cdcad6247fa03eca65cb34a4086497f3f
ETU\d.harle <damien.harle@gmail.com>
2022-12-05 15:58:44 +0100

    SpeedPad

--- Discosmos/Assets/Scripts/PlayerController.cs
+++ Discosmos/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -8,7 +9,7 @@ public class PlayerController : MonoBehaviour
 {
     
     private NavMeshAgent agent;
-    [SerializeField] private MovementType movementType = MovementType.moveToClickWithNavMesh;
+    [SerializeField] private MovementType movementType = MovementType.MoveToClickWithNavMesh;
     [Range(0,1)][SerializeField] private float force; //from 0 to 1
     [Range(0, 20)] [SerializeField] public float baseSpeed;
     [SerializeField] private AnimationCurve speedCurve;
@@ -22,24 +23,148 @@ public class PlayerController : MonoBehaviour
     private float currentSpeed;
     
     private bool attacking =false;
+    
+    private bool speedPadTriggered = false;
+    private Transform speedPad;
+    [SerializeField] private float speedUp = 10;
+    [SerializeField] private float slowDown = 2;
+    [SerializeField] private float precisionAnglePad;
+    [SerializeField] private bool speedPadPositive = false;
+    [SerializeField] private float slowSpeedPadDuration = 0.5f;
+    [SerializeField] private float fastSpeedPadDuration = 2;
+    private float speedPadSpeed = 0;
+    private double speedPadTimer = 0;
+    private double time;
+    [SerializeField] private float speedPadLerp = 1;
+    
+    [SerializeField] private Canvas canvas;
+    
+    
+    private Camera _camera;
 
     private void Start()
     {
+        _camera = Camera.main;
         agent = GetComponent<NavMeshAgent>();
         agent.speed = currentSpeed;
+        //if photon is on, then we are in multiplayer
+        SetTime();
     }
-    
+
+    private void SetTime()
+    {
+        if (PhotonNetwork.IsConnected)
+        {
+            time = PhotonNetwork.Time;
+        }
+        else
+        {
+            time = Time.deltaTime;
+        }
+    }
+
     private void Update()
     {
+        SetTime();
+        
         direction = transform.forward;
-        currentSpeed = (speedCurve.Evaluate(force) - slowDownCurve.Evaluate(force)) + baseSpeed;
+        currentSpeed = (speedCurve.Evaluate(force) - slowDownCurve.Evaluate(force)) + baseSpeed + speedPadSpeed;
         ClampSpeed();
+        agent.speed = currentSpeed;
 
 
         MovementTypeSwitch();
-        agent.speed = currentSpeed;
         Debug.DrawLine(transform.position, agent.destination, Color.yellow);
+
+        SpeedPadFunction();
+
+        //on the canvas, create a chart that shows the speed of the player 
+        canvas.transform.GetChild(0).GetComponent<RectTransform>().sizeDelta = new Vector2(currentSpeed * 10, 10);
     }
+
+    #region SpeedPad
+    private void SpeedPadFunction()
+    {
+        if (speedPadTriggered)
+        {
+            SpeedPadEffect();
+            speedPadTimer = 0;
+        }
+
+        if (!speedPadTriggered && speedPadSpeed != 0)
+        {
+            speedPadTimer += time;
+            //if the speedPadSpeed is positive, then use the duration of the fast speed pad
+            if (speedPadSpeed > 0)
+            {
+                if (speedPadTimer >= fastSpeedPadDuration)
+                {
+                    GoBackToNormalSpeed();
+                }
+            }
+            //if the speedPadSpeed is negative, then use the duration of the slow speed pad
+            else
+            {
+                if (speedPadTimer >= slowSpeedPadDuration)
+                {
+                    GoBackToNormalSpeed();
+                }
+            }
+        }
+    }
+
+    private void GoBackToNormalSpeed()
+    {
+        speedPadSpeed = Mathf.Lerp(speedPadSpeed, 0, speedPadLerp);
+    }
+    
+    public void SpeedPadTrigger(bool isTriggered, Transform speedPadTransform)
+    {
+        speedPadTriggered = isTriggered;
+        if (speedPadTriggered)
+        {
+            speedPad = speedPadTransform;
+        }
+        else
+        {
+            speedPad = null;
+        }
+    }
+    
+    //if the speed pad is triggered get the transform of the speed pad
+    private void SpeedPadEffect()
+    {
+        //if the player is going approximately in the same direction as the speed pad and not in the opposite direction
+        if (Vector3.Angle(transform.forward, speedPad.forward) < precisionAnglePad)
+        {
+            SpeedUpPad();
+        }
+        else
+        {
+            SlowDownPad();
+        }
+        //debug Vector3.Angle(transform.forward, speedPad.forward) in the scene view
+        Debug.DrawLine(transform.position, transform.position + transform.forward * 10, Color.red);
+        Debug.DrawLine(transform.position, transform.position + speedPad.forward * 10, Color.green);
+        Debug.DrawLine(transform.position, transform.position + Quaternion.AngleAxis(precisionAnglePad, Vector3.up) * transform.forward * 10, Color.blue);
+        
+    }
+
+    private void SlowDownPad()
+    {
+        speedPadSpeed = -slowDown;
+        speedPadPositive = false;
+        Debug.Log("slow down");
+    }
+
+    private void SpeedUpPad()
+    {
+        speedPadSpeed = speedUp;
+        speedPadPositive = true;
+        Debug.Log("speed up");
+    }
+    
+    #endregion
 
     private void OnDrawGizmos()
     {
@@ -50,25 +175,25 @@ public class PlayerController : MonoBehaviour
     
     private enum MovementType
     {
-        moveToClickWithNavMesh,
+        MoveToClickWithNavMesh,
         KeepDirectionWithoutNavMesh,
-        slide,
+        Slide,
     }
     
     
     
     private void MovementTypeSwitch()
     {
-        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (_camera != null) ray = _camera.ScreenPointToRay(Input.mousePosition);
         switch (movementType)
         {
-            case MovementType.moveToClickWithNavMesh:
+            case MovementType.MoveToClickWithNavMesh:
                 MoveToClickUsingTheNavMesh();
                 break;
             case MovementType.KeepDirectionWithoutNavMesh:
                 KeepDirectionNoNavMesh();
                 break;
-            case MovementType.slide:
+            case MovementType.Slide:
                 Slide();
                 break;
         }
@@ -116,4 +241,5 @@ public class PlayerController : MonoBehaviour
             currentSpeed = 0;
         }
     }
+    
 }
--- Discosmos/Assets/Scripts/SpeedPad/Speedpad.cs
+++ Discosmos/Assets/Scripts/SpeedPad/Speedpad.cs
@@ -5,39 +5,25 @@ using UnityEngine;
 public class Speedpad : MonoBehaviour
 {
     //make it so when an object enters the trigger, it checks if it has the PlayerController script and put speed to 10 if the player goes in the same direction the speedpad is facing
-    
-    [SerializeField] private float speed = 10;
-    [SerializeField] private float slow = 2;
-    
-    [SerializeField] private float precision;
-    
-    private float playerspeed;
+    [SerializeField] private PlayerController playerController;
     private void OnTriggerEnter(Collider other)
     {
         //draw the transform.forward of the speedpad
-        Debug.DrawRay(transform.position, transform.forward, Color.blue, 50);
-        Debug.Log("Enter");
         if (other.gameObject.GetComponent<PlayerController>())
         {
-            playerspeed = other.gameObject.GetComponent<PlayerController>().baseSpeed;
-            //if the player is approaching the speedpad from the same direction as the speedpad is facing, increase the speed
-            if (Vector3.Dot(other.gameObject.transform.forward, transform.forward) > precision)
-            {
-                other.gameObject.GetComponent<PlayerController>().baseSpeed *= speed;
-            }
-            else
-            {
-                other.gameObject.GetComponent<PlayerController>().baseSpeed *= 1/slow;
-            }
+            Debug.Log("Enter");
+            playerController = other.gameObject.GetComponent<PlayerController>();
+            playerController.SpeedPadTrigger(true,transform);
+            
         }
     }
     
     private void OnTriggerExit(Collider other)
     {
-        Debug.Log("Exit");
         if (other.gameObject.GetComponent<PlayerController>())
         {
-            other.gameObject.GetComponent<PlayerController>().baseSpeed = playerspeed;
+            Debug.Log("Exit");
+            playerController.SpeedPadTrigger(false,transform);
         }
     }
 }
