759d2fbaf5bc721761b343195b75d6de8f1e77eb
Mathis Jibert <mathis.jibert@gmail.com>
2022-12-13 16:01:41 +0100

    Push FOW

--- Discosmos/Assets/FieldOfViewGenerator.cs
+++ Discosmos/Assets/FieldOfViewGenerator.cs
@@ -20,20 +20,20 @@ public class FieldOfViewGenerator : MonoBehaviour
     private void Update()
     {
         List<Vector3> vertices = new List<Vector3>(0);
-        vertices.Add(transform.position);
-        List<int> triangles = null;
+        vertices.Add(Vector3.zero);
+        List<int> triangles = new List<int>(0);
         for (int i = 0; i < precision; i++)
         {
             Ray ray = new Ray(transform.position, Quaternion.Euler(0, 360 / precision * i, 0) * Vector3.left);
             if (Physics.Raycast(ray, out RaycastHit hit, lenght))
             {
                 Debug.DrawRay(ray.origin,ray.direction*hit.distance);
-                vertices.Add(hit.point);
+                vertices.Add(transform.InverseTransformPoint(hit.point));
             }
             else
             {
                 Debug.DrawRay(ray.origin,ray.direction*lenght);
-                vertices.Add(ray.origin+ray.direction*lenght);
+                vertices.Add(Vector3.zero+ray.direction*lenght);
             }
         }
 
@@ -46,5 +46,7 @@ public class FieldOfViewGenerator : MonoBehaviour
         mesh.vertices = vertices.ToArray();
         mesh.triangles = triangles.ToArray();
         mesh.RecalculateNormals();
+        mesh.RecalculateBounds();
+        meshFilter.mesh = mesh;
     }
 }
